fd6c5b44c37875bcb76be0bf9d9c21eafd969688
Dastinss <sergeyfdaragan@gmail.com>
2023-04-02 20:06:00 +0300

    #1 done without HW

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -8,10 +8,11 @@ const Skills = () => {
         <div className={style.skillsBlock}>
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
                 <h2 className={style.title}>Skills</h2>
-                <div className={style.skills}></div>
-                <Skill/>
-                <Skill/>
-                <Skill/>
+                <div className={style.skills}>
+                    <Skill title={'JS'} description={'some text about JS'}/>
+                    <Skill title={'CSS'} description={'some text about CSS'}/>
+                    <Skill title={'React'} description={'some text about React'}/>
+                </div>
             </div>
         </div>
     );
