98f3311d77faee822362f5ce1a923c42981c8c63
Dastinss <sergeyfdaragan@gmail.com>
2023-10-16 21:44:02 +0300

    #4th done myself. Fixed Skill block, added HomeSvgSelector (for icons) but it don`t work

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -3,23 +3,27 @@ import style from './Skills.module.scss'
 // import styleContainer from "../common/styles/Container.module.css";
 import Skill from "./skill/Skill";
 import Title from "../common/components/title/Title";
-import jsImage from './../assets/image/js-png.png'
+import jsImage from './../assets/image/jsImage.svg'
 import cssImage from './../assets/image/cssImage.svg'
 import reactImage from './../assets/image/reactImage.svg'
 
 const Skills = () => {
 
-    const js = {
-        iconImage: `url(${jsImage})` ,
-    };
+    // const js = {
+    //     iconImage: `url(${jsImage})` ,
 
-    const css = {
-        iconImage: `url(${cssImage})` ,
-    };
-
+    // };
+    //
+    // const css = {
+    //     iconImage: `url(${cssImage})` ,
+    // };
+    //
     const react = {
-        iconImage: `url(${reactImage})` ,
+        // iconImage: `url(${reactImage})` ,
+        iconImage: reactImage
     };
+
+
 
 
 
@@ -30,8 +34,8 @@ const Skills = () => {
             {/*<div className={`${style.container} ${style.skillsContainer}`}>*/}
                 <Title text={'Skills'}/>
                 <div className={style.skills}>
-                    <Skill image={js} title={'JS'} description={'some text about JS'}/>
-                    <Skill image={css} title={'CSS'} description={'some text about CSS'}/>
+                    <Skill image={react} title={'JS'} description={'some text about JS'}/>
+                    <Skill image={react} title={'HTML % CSS'} description={'some text about CSS'}/>
                     <Skill image={react} title={'React'} description={'some text about React'}    />
                     {/*<Skill title={'React'} description={'some text about React'}/>*/}
                 </div>
